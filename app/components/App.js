e9ba28bce5c857fc634c96c59e96bed236aac71f
Jonathan Nguyen <jnguyen7410@gmail.com>
2019-06-01 04:38:48 -0700

    Created a component and a time waster :)

--- app/components/App.js
+++ app/components/App.js
@@ -1,1 +1,57 @@
 import React, { Component } from 'react'
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date(), counter: 0};
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+        this.setState((state, props) => ({
+            // counter: state.counter + props.increment
+            counter: state.counter + 1
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <h3>Psst...you wasted {this.state.counter} seconds on this page. Silly you :)</h3>
+            </div>
+        );
+    }
+}
+
+
+function Welcome(props) {
+    return <h2>Hello {props.name}</h2>;
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>NguyeningCode</h1>
+                <Welcome name="Jurytan" />
+                <Clock/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,28 +1,55 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var App = require('./components/App');
-var name = 'Jurytan';
-let maow = 1 + 2 - 3 + 4;
-const blah = <h2>This is a test of the feature. Maow = {maow}</h2>;
+import App from './components/App';
 
-// ReactDOM.render( <
-//     App / > ,
-//     document.getElementById('app')
+
+// let shouldHide = "hidden";
+// const user = {
+//     firstName: 'Jurytan',
+//     lastName: 'Nguyen',
+//     idNum: 1
+// };
+
+// function formatName (user) {
+//     return user.firstName + ' ' + user.lastName;
+// }
+
+// // const blah = <h2 hidden="hidden">This is a test of the feature. Jurytan's ID = {user.idNum}</h2>;
+// const blah = <h2 hidden={shouldHide}>This is a test of the feature. Jurytan's ID = {user.idNum}</h2>;
+
+// // ReactDOM.render( <
+// //     App / > ,
+// //     document.getElementById('app')
+// // );
+
+// const element = React.createElement(
+//     'h1',
+//     {className: 'greeting'},
+//     'Hello World for the second time!'
 // );
 
+
+
+// // ReactDOM.render(
+// //     <body>
+// //         <h1>Hello World!</h1>
+// //         <p>This is me adding another tag</p>
+// //         <ul>
+// //             <li>Test 1</li>
+// //             <li>Test 2</li>
+// //             <li>Test 3</li>
+// //             <li>Test 4</li>
+// //             <li>Test 5</li> 
+// //         </ul>
+// //         <h3>This page belongs to {formatName(user)}</h3>
+// //         {blah}
+// //         {element}
+// //         <div id="test"></div>
+// //     </body>,
+// //     document.getElementById('root')
+// // );
+
 ReactDOM.render(
-    <body>
-        <h1>Hello World!</h1>
-        <p>This is me adding another tag</p>
-        <ul>
-            <li>Test 1</li>
-            <li>Test 2</li>
-            <li>Test 3</li>
-            <li>Test 4</li>
-            <li>Test 5</li> 
-        </ul>
-        <h3>This page belongs to {name}</h3>
-        {blah}
-    </body>,
-    document.getElementById('app')
+    <App />,
+    document.getElementById('root')
 );
